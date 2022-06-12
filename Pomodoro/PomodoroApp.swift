414eae1adf52f8af353ae0cd202534c5ea35a83f
po-gl <porterglines@gmail.com>
2022-06-12 16:36:37 -0600

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Porter Glines on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
