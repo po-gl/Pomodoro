d54ec053a6813a5f9aed800d912c1a3b1c8ba40a
po-gl <porterglines@gmail.com>
2022-10-22 21:22:56 -0600

    Use CoreHaptics for timer endings

--- Pomodoro/Views/ContentView.swift
+++ Pomodoro/Views/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ContentView: View {
     @ObservedObject var pomoTimer: PomoTimer
@@ -40,8 +41,12 @@ struct ContentView: View {
             }
             .background(pomoTimer.isPaused ? Color("BackgroundStopped") : backgroundActiveColor)
             .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification), perform: {_ in
-                pomoTimer.saveToUserDefaults()
+//                pomoTimer.saveToUserDefaults()
             })
+            .onAppear {
+                getNotificationPermissions()
+                prepareHaptics()
+            }
             .onChange(of: pomoTimer.status) { _ in
                 handleTimerEnd()
             }
@@ -61,18 +66,8 @@ struct ContentView: View {
             }
         }
         
-        // Asking permission
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-            if success {
-                print("All set on permissions!")
-            } else if let error = error {
-                print(error.localizedDescription)
-            }
-        }
-        
         // Vibration?
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.success)
+        workStart()
         
         // Notification
         let content = UNMutableNotificationContent()
@@ -88,6 +83,47 @@ struct ContentView: View {
 
         // add our notification request
         UNUserNotificationCenter.current().add(request)
+    }
+    
+    
+    func getNotificationPermissions() {
+        // Asking permission
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { success, error in
+            if success {
+                print("All set on permissions!")
+            } else if let error = error {
+                print("There was an error requesting permissions: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the haptics engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func workStart() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events: [CHHapticEvent] = []
+        
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1.0)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1.0)
+        events.append(CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0.0))
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0.0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription)")
+        }
     }
 }
 
