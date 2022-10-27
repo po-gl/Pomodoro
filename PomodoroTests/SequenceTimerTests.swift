f80389d04d34212733aa0c80af26278da6e0e526
po-gl <porterglines@gmail.com>
2022-10-26 23:00:53 -0600

    Update unit tests for SequenceTimer changes

--- PomodoroTests/SequenceTimerTests.swift
+++ PomodoroTests/SequenceTimerTests.swift
@@ -9,66 +9,68 @@ import XCTest
 
 final class SequenceTimerTests: XCTestCase {
     
-    var sequenceTimer = SequenceTimer(sequenceOfIntervals: [], timerProvider: MockTimer.self)
+    var sequenceTimer = SequenceTimer([], perform: { _ in return }, timerProvider: MockTimer.self)
+    var actionsPerformed = 0
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         let intervals = [2.0, 3.0]
-        sequenceTimer = SequenceTimer(sequenceOfIntervals: intervals, timerProvider: MockTimer.self)
+        actionsPerformed = 0
+        sequenceTimer = SequenceTimer(intervals, perform: { i in
+            self.actionsPerformed += 1
+        }, timerProvider: MockTimer.self)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        sequenceTimer.end()
     }
 
     func testSequenceTimerInitializer() throws {
-        XCTAssertEqual(sequenceTimer.currentIndex, 0)
-        XCTAssertEqual(sequenceTimer.timeRemaining, 2.0)
+        let now = Date()
+        XCTAssertEqual(sequenceTimer.getIndex(atDate: now), 0)
+        XCTAssertEqual(sequenceTimer.timeRemaining(atDate: now), 2.0)
     }
-    
+
     func testInIntervalOne_AfterOneSecond() throws {
-        passTime(seconds: 1)
-        XCTAssertEqual(sequenceTimer.currentIndex, 0)
-        XCTAssertEqual(sequenceTimer.timeRemaining, 1.0)
+        let now = Date().addingTimeInterval(1.0)
+        XCTAssertEqual(sequenceTimer.getIndex(atDate: now), 0)
+        XCTAssertEqual(sequenceTimer.timeRemaining(atDate: now), 1.0)
     }
-    
+
     func testInIntervalOne_AfterTwoSeconds() throws {
-        passTime(seconds: 2)
-        XCTAssertEqual(sequenceTimer.currentIndex, 0)
-        XCTAssertEqual(sequenceTimer.timeRemaining, 0.0)
+        let now = Date().addingTimeInterval(2.0)
+        XCTAssertEqual(sequenceTimer.getIndex(atDate: now), 0)
+        XCTAssertEqual(sequenceTimer.timeRemaining(atDate: now), 0.0)
     }
-    
+
     func testInIntervalTwo() throws {
-        passTime(seconds: 3)
-        XCTAssertEqual(sequenceTimer.currentIndex, 1)
-        XCTAssertEqual(sequenceTimer.timeRemaining, 3.0)
+        let now = Date().addingTimeInterval(3.0)
+        XCTAssertEqual(sequenceTimer.getIndex(atDate: now), 1)
+        XCTAssertEqual(sequenceTimer.timeRemaining(atDate: now), 3.0)
     }
-    
+
     func testInIntervalTwo_atEnd() throws {
-        passTime(seconds: 6)
-        XCTAssertEqual(sequenceTimer.currentIndex, 1)
-        XCTAssertEqual(sequenceTimer.timeRemaining, 0.0)
+        let now = Date().addingTimeInterval(6.0)
+        XCTAssertEqual(sequenceTimer.getIndex(atDate: now), 1)
+        XCTAssertEqual(sequenceTimer.timeRemaining(atDate: now), 0.0)
     }
-    
-    func testRepeatingTimer() throws {
-        passTime(seconds: 8)
-        XCTAssertEqual(sequenceTimer.currentIndex, 0)
-        XCTAssertEqual(sequenceTimer.timeRemaining, 1.0)
+
+    func testPastEnd() throws {
+        let now = Date().addingTimeInterval(8.0)
+        XCTAssertEqual(sequenceTimer.getIndex(atDate: now), 1)
+        XCTAssertEqual(sequenceTimer.timeRemaining(atDate: now), 0.0)
     }
-    
     
     func testPauseTimer() throws {
         sequenceTimer.pause()
         XCTAssertTrue(sequenceTimer.isPaused)
     }
-    
+
     func testResetTimer() throws {
-        passTime(seconds: 5)
+        passTime(seconds: 8)
         sequenceTimer.reset()
-        passTime(seconds: 1)
-        XCTAssertEqual(sequenceTimer.currentIndex, 0)
-        XCTAssertEqual(sequenceTimer.timeRemaining, 2.0)
+        passTime(seconds: 3)
+        XCTAssertEqual(actionsPerformed, 3)
     }
     
     
@@ -92,9 +94,14 @@ class MockTimer: Timer {
     var block: ((Timer) -> Void)!
     
     static var currentTimer: MockTimer!
+    static var timeRemaining: TimeInterval!
     
     override func fire() {
-        block(self)
+        if MockTimer.timeRemaining == 0.0 {
+            block(self)
+        } else {
+            MockTimer.timeRemaining -= 1.0
+        }
     }
     
     override func invalidate() {
@@ -104,6 +111,7 @@ class MockTimer: Timer {
         let mockTimer = MockTimer()
         mockTimer.block = block
         self.currentTimer = mockTimer
+        self.timeRemaining = withTimeInterval
         return mockTimer
     }
 }
