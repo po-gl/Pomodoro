09763035c8fd0ab2c7f00de66b9aeb50d668c336
po-gl <porterglines@gmail.com>
2022-10-30 15:59:54 -0600

    Add simplified watchOS complication with just status

--- WatchWidget/WatchWidget.swift
+++ WatchWidget/WatchWidget.swift
@@ -10,6 +10,9 @@ import SwiftUI
 import Intents
 
 struct Provider: IntentTimelineProvider {
+    
+    let shouldAddMinuteByMinuteEntries: Bool
+    
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(),
                     status: .work,
@@ -33,9 +36,15 @@ struct Provider: IntentTimelineProvider {
         var entries: [SimpleEntry] = []
         let pomoTimer = PomoTimer()
         pomoTimer.restoreFromUserDefaults()
+        
+        let now = Date()
+        addEntry(for: now, &entries, configuration, pomoTimer)
 
         addTransitionEntries(&entries, configuration, pomoTimer)
-        addByMinuteEntries(&entries, configuration, pomoTimer)
+        
+        if shouldAddMinuteByMinuteEntries {
+            addByMinuteEntries(&entries, configuration, pomoTimer)
+        }
 
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
@@ -50,7 +59,7 @@ struct Provider: IntentTimelineProvider {
     private func addTransitionEntries(_ entries: inout [SimpleEntry], _ configuration: ConfigurationIntent, _ pomoTimer: PomoTimer) {
         let currentDate = Date()
         var runningTime = 0.0
-        for i in 0..<pomoTimer.pomoCount * 2 {
+        for i in 0..<(pomoTimer.pomoCount-1) * 2 {
             let workOrRest = i % 2 == 0 ? work : rest
             runningTime += workOrRest + 1.0
             
@@ -58,7 +67,7 @@ struct Provider: IntentTimelineProvider {
             addEntry(for: entryDate, &entries, configuration, pomoTimer)
         }
         
-        runningTime += breakTime
+        runningTime += breakTime + 1.0
         let entryDate = currentDate.addingTimeInterval(runningTime)
         addEntry(for: entryDate, &entries, configuration, pomoTimer)
     }
@@ -69,7 +78,7 @@ struct Provider: IntentTimelineProvider {
         let totalMinutes = totalSeconds / 60.0
         
         let currentDate = Date()
-        for timeOffset in 0 ..< Int(totalMinutes)+1 {
+        for timeOffset in 0 ..< Int(totalMinutes)-1 {
             let entryDate = Calendar.current.date(byAdding: .minute, value: timeOffset, to: currentDate)!
             addEntry(for: entryDate, &entries, configuration, pomoTimer)
         }
@@ -85,11 +94,11 @@ struct Provider: IntentTimelineProvider {
     }
 
     func recommendations() -> [IntentRecommendation<ConfigurationIntent>] {
-        return [
-            IntentRecommendation(intent: ConfigurationIntent(), description: "Emoji Progress")
-        ]
+        let description = shouldAddMinuteByMinuteEntries ? "Status with Progress" : "Status"
+        return [ IntentRecommendation(intent: ConfigurationIntent(), description: description) ]
     }
 }
+
 
 struct SimpleEntry: TimelineEntry {
     var date: Date
@@ -99,63 +108,103 @@ struct SimpleEntry: TimelineEntry {
 }
 
 
-struct WatchWidgetEntryView : View {
+struct ProgressWidgetView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        ProgressView(value: entry.timeRemaining, total: getTotalForStatus(entry.status)) {
-            Text("\(getIconForStatus(entry.status))")
-                .font(.system(size: 20))
-        }
+        ZStack {
+            AccessoryWidgetBackground()
+            ProgressView(value: entry.timeRemaining, total: getTotalForStatus(entry.status)) {
+                Text("\(getIconForStatus(entry.status))")
+                    .font(.system(size: 20))
+            }
             .progressViewStyle(.circular)
-    }
-    
-    
-    func getIconForStatus(_ status: PomoStatus) -> String {
-        switch status{
-        case .work:
-            return "🌶️"
-        case .rest:
-            return "🍇"
-        case .longBreak:
-            return "🏖️"
-        case .end:
-            return "🎉"
-        }
-    }
-    
-    func getTotalForStatus(_ status: PomoStatus) -> Double {
-        switch status{
-        case .work:
-            return PomoTimer.defaultWorkTime
-        case .rest:
-            return PomoTimer.defaultRestTime
-        case .longBreak:
-            return PomoTimer.defaultBreakTime
-        case .end:
-            return 1.0
+            .widgetAccentable()
         }
     }
 }
 
+struct StatusWidgetView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        ZStack {
+            AccessoryWidgetBackground()
+            Text("\(getIconForStatus(entry.status))")
+                .font(.system(size: 20))
+            .progressViewStyle(.circular)
+            .widgetAccentable()
+        }
+    }
+}
+
+
+fileprivate func getIconForStatus(_ status: PomoStatus) -> String {
+    switch status{
+    case .work:
+        return "🌶️"
+    case .rest:
+        return "🍇"
+    case .longBreak:
+        return "🏖️"
+    case .end:
+        return "🎉"
+    }
+}
+
+fileprivate func getTotalForStatus(_ status: PomoStatus) -> Double {
+    switch status{
+    case .work:
+        return PomoTimer.defaultWorkTime
+    case .rest:
+        return PomoTimer.defaultRestTime
+    case .longBreak:
+        return PomoTimer.defaultBreakTime
+    case .end:
+        return 1.0
+    }
+}
+
+
 @main
-struct WatchWidget: Widget {
-    let kind: String = "WatchWidget"
+struct PomoWidgets: WidgetBundle {
+    var body: some Widget {
+        StatusWatchWidget()
+        ProgressWatchWidget()
+    }
+}
+
+struct ProgressWatchWidget: Widget {
+    let kind: String = "ProgressWatchWidget"
 
     var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
-            WatchWidgetEntryView(entry: entry)
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider(shouldAddMinuteByMinuteEntries: true)) { entry in
+            ProgressWidgetView(entry: entry)
         }
-        .configurationDisplayName("Pomodoro Status")
+        .configurationDisplayName("Pomodoro Progress")
         .description("Track your pomodoro timer.")
     }
 }
+
+
+struct StatusWatchWidget: Widget {
+    let kind: String = "StatusWatchWidget"
+
+    var body: some WidgetConfiguration {
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider(shouldAddMinuteByMinuteEntries: false)) { entry in
+            StatusWidgetView(entry: entry)
+        }
+        .configurationDisplayName("Pomodoro Status")
+        .description("See your pomodoro timer status.")
+    }
+}
+
 
 struct WatchWidget_Previews: PreviewProvider {
     static var pomoTimer = PomoTimer(pomos: 2, longBreak: PomoTimer.defaultBreakTime, perform: { _ in return })
     
     static var previews: some View {
-        WatchWidgetEntryView(entry: SimpleEntry(date: Date(), status: .work, timeRemaining: PomoTimer.defaultWorkTime, configuration: ConfigurationIntent()))
+        ProgressWidgetView(entry: SimpleEntry(date: Date(), status: .work, timeRemaining: PomoTimer.defaultWorkTime, configuration: ConfigurationIntent()))
             .previewContext(WidgetPreviewContext(family: .accessoryCircular))
     }
 }
