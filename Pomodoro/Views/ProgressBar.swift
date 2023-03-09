aac0860962dcc18f12c349c5bae0d6f1c5cbde1b
po-gl <porterglines@gmail.com>
2023-03-09 11:59:11 -0700

    Refactor code structure

--- Pomodoro/Views/ProgressBar.swift
+++ Pomodoro/Views/ProgressBar.swift
@@ -35,25 +35,6 @@ struct ProgressBar: View {
             }
     }
     
-    var drag: some Gesture {
-        DragGesture(minimumDistance: 0.0, coordinateSpace: .local)
-            .onChanged { event in
-                guard pomoTimer.isPaused || pomoTimer.getStatus() == .end else { return }
-                if !dragStarted { heavyHaptic() }
-                
-                isDragging = true; dragStarted = true
-                let padding = barPadding + barOutlinePadding
-                
-                var x = event.location.x.rounded()
-                x = x.clamped(to: padding...metrics.size.width - padding)
-                x -= padding
-                
-                let percent = x / getBarWidth()
-                pomoTimer.setPercentage(to: percent)
-            }
-            .onEnded { _ in dragStarted = false }
-    }
-    
     @ViewBuilder
     private func TimeLineColorBars() -> some View {
         TimelineView(PeriodicTimelineSchedule(from: Date(), by: 1.0)) { context in
@@ -70,9 +51,8 @@ struct ProgressBar: View {
                 ZStack {
                     ColorBars()
                         .mask { RoundedRectangle(cornerRadius: 8) }
-                    if pomoTimer.getProgress(atDate: context.date) != 0.0 || !pomoTimer.isPaused || isDragging {
-                        ProgressIndicator(at: context.date)
-                    }
+                    ProgressIndicator(at: context.date)
+                        .opacity(shouldShowProgressIndicator(at: context.date) ? 1.0 : 0.0)
                 }
                 .padding(.vertical, 2)
                 .padding(.horizontal, barOutlinePadding)
@@ -84,41 +64,6 @@ struct ProgressBar: View {
             .padding(.horizontal)
         }
     }
-    
-    
-    func getProportion(_ index: Int) -> Double {
-        let intervals = pomoTimer.order.map { $0.getTime() }
-        let total = intervals.reduce(0, +)
-        return intervals[index] / total
-    }
-    
-    
-    func getColorForStatus(_ status: PomoStatus) -> LinearGradient {
-        switch status {
-        case .work:
-            return LinearGradient(stops: [.init(color: Color("BarWork"), location: 0.5),
-                                          .init(color: Color(hex: 0xD3EDDD), location: 1.1)],
-                                  startPoint: .leading, endPoint: .trailing)
-        case .rest:
-            return LinearGradient(stops: [.init(color: Color("BarRest"), location: 0.2),
-                                          .init(color: Color(hex: 0xE8BEB1), location: 1.0)],
-                                  startPoint: .leading, endPoint: .trailing)
-        case .longBreak:
-            return LinearGradient(stops: [.init(color: Color("BarLongBreak"), location: 0.5),
-                                          .init(color: Color(hex: 0xF5E1E1), location: 1.3)],
-                                  startPoint: .leading, endPoint: .trailing)
-        case .end:
-            return LinearGradient(stops: [.init(color: Color("End"), location: 0.5),
-                                          .init(color: Color(hex: 0xD3EDDD), location: 1.1)],
-                                  startPoint: .leading, endPoint: .trailing)
-        }
-    }
-    
-    
-    func getBarWidth() -> Double {
-        return metrics.size.width - barPadding*2 - barOutlinePadding*2
-    }
-    
     
     @ViewBuilder
     private func ColorBars() -> some View {
@@ -145,5 +90,64 @@ struct ProgressBar: View {
         }.mask {
             ColorBars()
         }
+    }
+    
+    private func shouldShowProgressIndicator(at date: Date) -> Bool {
+        return pomoTimer.getProgress(atDate: date) != 0.0 || !pomoTimer.isPaused || isDragging
+    }
+    
+    
+    private var drag: some Gesture {
+        DragGesture(minimumDistance: 0.0, coordinateSpace: .local)
+            .onChanged { event in
+                guard pomoTimer.isPaused || pomoTimer.getStatus() == .end else { return }
+                if !dragStarted { heavyHaptic() }
+                
+                isDragging = true; dragStarted = true
+                let padding = barPadding + barOutlinePadding
+                
+                var x = event.location.x.rounded()
+                x = x.clamped(to: padding...metrics.size.width - padding)
+                x -= padding
+                
+                let percent = x / getBarWidth()
+                pomoTimer.setPercentage(to: percent)
+            }
+            .onEnded { _ in
+                dragStarted = false
+            }
+    }
+    
+    
+    private func getProportion(_ index: Int) -> Double {
+        let intervals = pomoTimer.order.map { $0.getTime() }
+        let total = intervals.reduce(0, +)
+        return intervals[index] / total
+    }
+    
+    
+    private func getColorForStatus(_ status: PomoStatus) -> LinearGradient {
+        switch status {
+        case .work:
+            return LinearGradient(stops: [.init(color: Color("BarWork"), location: 0.5),
+                                          .init(color: Color(hex: 0xD3EDDD), location: 1.1)],
+                                  startPoint: .leading, endPoint: .trailing)
+        case .rest:
+            return LinearGradient(stops: [.init(color: Color("BarRest"), location: 0.2),
+                                          .init(color: Color(hex: 0xE8BEB1), location: 1.0)],
+                                  startPoint: .leading, endPoint: .trailing)
+        case .longBreak:
+            return LinearGradient(stops: [.init(color: Color("BarLongBreak"), location: 0.5),
+                                          .init(color: Color(hex: 0xF5E1E1), location: 1.3)],
+                                  startPoint: .leading, endPoint: .trailing)
+        case .end:
+            return LinearGradient(stops: [.init(color: Color("End"), location: 0.5),
+                                          .init(color: Color(hex: 0xD3EDDD), location: 1.1)],
+                                  startPoint: .leading, endPoint: .trailing)
+        }
+    }
+    
+    private func getBarWidth() -> Double {
+        return metrics.size.width - barPadding*2 - barOutlinePadding*2
     }
 }
