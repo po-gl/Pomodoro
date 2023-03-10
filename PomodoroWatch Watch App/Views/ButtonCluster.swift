9a9e1033c510cf58fd639652bfe5d7a22cc0bc37
po-gl <porterglines@gmail.com>
2023-03-10 16:11:33 -0700

    Update the watchOS start/stop/reset button logic to match iOS logic

--- PomodoroWatch Watch App/Views/ButtonCluster.swift
+++ PomodoroWatch Watch App/Views/ButtonCluster.swift
@@ -15,33 +15,46 @@ struct ButtonCluster: View {
     
     var body: some View {
         HStack {
-            Image(systemName: withFill("arrow.counterclockwise.circle"))
-                .accessibilityIdentifier("resetButton")
-                .foregroundColor(pomoTimer.isPaused ? .orange : Color(hex: 0x333333))
-                .font(.system(size: 40))
-                .onTapGesture {
-                    resetHaptic()
-                    pomoTimer.unpause() // to update crown scroll progress
-                    withAnimation(.easeIn(duration: 0.2)){
-                        pomoTimer.reset()
-                        pomoTimer.pause()
-                    }
-                }
-                .disabled(!pomoTimer.isPaused)
+            ResetButton()
             Spacer()
-            
-            Image(systemName: pomoTimer.isPaused ? withFill("play.circle") : withFill("pause.circle"))
-                .accessibilityIdentifier("playPauseButton")
-                .foregroundColor(.accentColor)
-                .font(.system(size: 40))
-                .onTapGesture {
-                    pomoTimer.isPaused ? startHaptic() : stopHaptic()
-                    withAnimation(.easeIn(duration: 0.2)){
-                        pomoTimer.toggle()
-                    }
-                }
-                .disabled(pomoTimer.getStatus() == .end)
+            StartStopButton()
         }
+    }
+    
+    @ViewBuilder
+    private func ResetButton() -> some View {
+        let isEnabled = pomoTimer.isPaused || pomoTimer.getStatus() == .end
+        Image(systemName: withFill("arrow.counterclockwise.circle"))
+            .accessibilityIdentifier("resetButton")
+            .foregroundColor(pomoTimer.isPaused ? .orange : Color(hex: 0x333333))
+            .font(.system(size: 40))
+            .onTapGesture {
+                guard isEnabled else { return }
+                resetHaptic()
+                pomoTimer.unpause() // to update crown scroll progress
+                withAnimation(.easeIn(duration: 0.2)){
+                    pomoTimer.reset()
+                    pomoTimer.pause()
+                }
+            }
+            .disabled(!isEnabled)
+    }
+    
+    @ViewBuilder
+    private func StartStopButton() -> some View {
+        let isEnabled = pomoTimer.getStatus() != .end
+        Image(systemName: pomoTimer.isPaused ? withFill("play.circle") : withFill("pause.circle"))
+            .accessibilityIdentifier("playPauseButton")
+            .foregroundColor(.accentColor)
+            .font(.system(size: 40))
+            .onTapGesture {
+                guard isEnabled else { return }
+                pomoTimer.isPaused ? startHaptic() : stopHaptic()
+                withAnimation(.easeIn(duration: 0.2)){
+                    pomoTimer.toggle()
+                }
+            }
+            .disabled(!isEnabled)
     }
     
     private func withFill(_ systemName: String) -> String {
