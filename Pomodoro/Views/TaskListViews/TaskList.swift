4d312468dbc62f3aefac74efd6a427a7d34f7aa1
po-gl <porterglines@gmail.com>
2023-05-31 19:25:24 -0600

    Change nav bar title to inline to avoid jumps in scrolling animations This appears to be inherit to SwiftUI and is likely to be fixed by Apple

--- Pomodoro/Views/TaskListViews/TaskList.swift
+++ Pomodoro/Views/TaskListViews/TaskList.swift
@@ -59,7 +59,7 @@ struct TaskList: View {
             }
         }
         .navigationTitle(dayFormatter.string(from: Date()))
-        .navigationBarTitleDisplayMode(.large)
+//        .navigationBarTitleDisplayMode(.large)
         
         .toolbar {
             Menu {
