a9f56678db8f4ba21d9ceaf5d3dd4b9e85d3faa0
po-gl <porterglines@gmail.com>
2023-06-15 11:27:55 -0600

    Change NavigationView to newer NavigationStack

--- Pomodoro/Views/ContentView.swift
+++ Pomodoro/Views/ContentView.swift
@@ -33,7 +33,7 @@ struct ContentView: View {
     }
     
     var body: some View {
-        NavigationView {
+        NavigationStack {
             MainPage()
                 .reverseStatusBarColor()
                 .ignoresSafeArea()
