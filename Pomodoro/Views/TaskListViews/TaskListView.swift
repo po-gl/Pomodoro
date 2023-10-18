cbc6913e0edcc5b1eafb75f9d9412b774bd7707e
po-gl <porterglines@gmail.com>
2023-10-17 19:49:52 -0600

    Connect diffable data source with Core Data

--- Pomodoro/Views/TaskListViews/TaskListView.swift
+++ Pomodoro/Views/TaskListViews/TaskListView.swift
@@ -6,42 +6,49 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct TaskListView: UIViewControllerRepresentable {
+    @Environment(\.managedObjectContext) private var viewContext
 
     func makeUIViewController(context: Context) -> UIViewController {
-        TaskListViewController()
+        TaskListViewController(viewContext: viewContext)
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
     }
 }
 
-private struct StaticData {
-    lazy var numbers = (0..<100).map { _ in DataNumber(val: Int.random(in: 1...32))}
-}
+class TaskListViewController: UIViewController, NSFetchedResultsControllerDelegate {
+    enum Section {
+        case tasks
+    }
+    private typealias ListItemID = NSManagedObjectID
+    private typealias ListItem = NSManagedObject
 
-private struct DataNumber: Hashable {
-    var val: Int
-    var id = UUID()
-}
+    private var collectionView: UICollectionView! = nil
+    private var diffableDataSource: UICollectionViewDiffableDataSource<Section, ListItemID>! = nil
 
-class TaskListViewController: UIViewController {
-    private typealias ListItem = DataNumber
-    private var data = StaticData()
+    private var todaysTasksController: NSFetchedResultsController<TaskNote>! = nil
+    private var viewContext: NSManagedObjectContext
 
-    private var collectionView: UICollectionView!
-    private var diffableDataSource: UICollectionViewDiffableDataSource<Int, ListItem>!
+    init(viewContext: NSManagedObjectContext) {
+        self.viewContext = viewContext
+        super.init(nibName: nil, bundle: nil)
+    }
 
-    override func loadView() {
-        super.loadView()
-        setUpCollectionView()
-        view = collectionView
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureLayout()
+        configureDataSource()
 
+        configureFetchController()
+
+        view = collectionView
         title = "Task List"
     }
 
@@ -50,28 +57,16 @@ class TaskListViewController: UIViewController {
         static let sectionSpacing = 10.0
     }
 
-    private func setUpCollectionView() {
+    private func configureLayout() {
         let layout = UICollectionViewCompositionalLayout { [unowned self] _, layoutEnvironment in
-            return createTaskSection(layoutEnvironment)
+            return createTasksLayout(layoutEnvironment)
         }
-
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.autoresizingMask = [.flexibleHeight]
         collectionView.allowsSelection = false
-
-        // swiftlint:disable:next line_length
-        let diffableDataSource = UICollectionViewDiffableDataSource<Int, ListItem>(collectionView: collectionView) { collectionView, indexPath, _ -> UICollectionViewCell? in
-            let item = self.data.numbers[indexPath.item]
-            let cell = collectionView.dequeueConfiguredReusableCell(using: self.taskCellRegistration,
-                                                                    for: indexPath,
-                                                                    item: item)
-            return cell
-        }
-        self.diffableDataSource = diffableDataSource
-        collectionView.dataSource = self.diffableDataSource
-        updateSnapshot()
     }
 
-    private func createTaskSection(_ layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
+    private func createTasksLayout(_ layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
         var config = UICollectionLayoutListConfiguration(appearance: .plain)
         config.backgroundColor = .clear
         let section = NSCollectionLayoutSection.list(using: config, layoutEnvironment: layoutEnvironment)
@@ -82,32 +77,122 @@ class TaskListViewController: UIViewController {
         return section
     }
 
+    private func configureDataSource() {
+        // swiftlint:disable:next line_length
+        let diffableDataSource = UICollectionViewDiffableDataSource<Section, ListItemID>(collectionView: collectionView) { collectionView, indexPath, identifier -> UICollectionViewCell? in
+            let item = self.viewContext.object(with: identifier)
+            let cell = collectionView.dequeueConfiguredReusableCell(using: self.taskCellRegistration,
+                                                                    for: indexPath,
+                                                                    item: item)
+            return cell
+        }
+        self.diffableDataSource = diffableDataSource
+        collectionView.dataSource = self.diffableDataSource
+    }
+
     private var taskCellRegistration: UICollectionView.CellRegistration<UICollectionViewCell, ListItem> = {
         .init { cell, _, item in
+            guard let taskItem = item as? TaskNote,
+                  let viewContext = taskItem.managedObjectContext else {
+                return
+            }
             cell.contentConfiguration = UIHostingConfiguration {
-                Cell(data: item.val)
+                Cell(data: taskItem.text ?? "", id: taskItem.id)
+                    .id(taskItem.id)
+                    .environment(\.managedObjectContext, viewContext)
             }
         }
     }()
 
-    private func updateSnapshot() {
-        var snapshot = NSDiffableDataSourceSnapshot<Int, ListItem>()
-        snapshot.appendSections([0])
-        snapshot.appendItems(data.numbers)
+    private func configureFetchController() {
+        let todaysTasksFetchRequest = NSFetchRequest<TaskNote>(entityName: "TaskNote")
+        todaysTasksFetchRequest.sortDescriptors = [
+            SortDescriptor(\TaskNote.order, order: .reverse),
+            SortDescriptor(\TaskNote.timestamp, order: .forward)
+        ].map { descriptor in NSSortDescriptor(descriptor) }
+        todaysTasksFetchRequest.predicate = NSPredicate(format: "timestamp >= %@ && timestamp <= %@",
+                                                        Calendar.current.startOfDay(for: Date()) as CVarArg,
+                                                        Calendar.current.startOfDay(for: Date() + 86400) as CVarArg)
 
-        diffableDataSource.apply(snapshot, animatingDifferences: true)
+        todaysTasksController = NSFetchedResultsController(fetchRequest: todaysTasksFetchRequest,
+                                                                managedObjectContext: viewContext,
+                                                                sectionNameKeyPath: nil,
+                                                                cacheName: nil)
+        todaysTasksController.delegate = self
+
+        do {
+            try todaysTasksController.performFetch()
+        } catch {
+            fatalError("Failed to fetch entities: \(error)")
+        }
     }
 }
 
+extension TaskListViewController {
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChangeContentWith snapshot: NSDiffableDataSourceSnapshotReference) {
+        // swiftlint:disable:next line_length
+        guard let dataSource = collectionView.dataSource as? UICollectionViewDiffableDataSource<Section, NSManagedObjectID> else {
+            assertionFailure("Collection view data source not available")
+            return
+        }
+
+        var snapshot = snapshot as NSDiffableDataSourceSnapshot<Section, NSManagedObjectID>
+        let currentSnapshot = dataSource.snapshot() as NSDiffableDataSourceSnapshot<Section, NSManagedObjectID>
+
+        let reloadIdentifiers: [NSManagedObjectID] = snapshot.itemIdentifiers.compactMap { itemIdentifier in
+            guard let index = snapshot.indexOfItem(itemIdentifier),
+                  let currentIndex = currentSnapshot.indexOfItem(itemIdentifier),
+                  index == currentIndex else {
+                return nil
+            }
+            // swiftlint:disable:next line_length
+            guard let existingObjects = try? todaysTasksController.managedObjectContext.existingObject(with: itemIdentifier),
+                  existingObjects.isUpdated else {
+                return nil
+            }
+            return itemIdentifier
+        }
+
+        snapshot.reloadItems(reloadIdentifiers)
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+}
+
+#if DEBUG
+// swiftlint:disable:next line_length
+final class DebugDiffableDataSource<SectionIdentifier, ItemIdentifier>: UICollectionViewDiffableDataSource<SectionIdentifier, ItemIdentifier> where SectionIdentifier: Hashable, ItemIdentifier: Hashable {
+
+    // swiftlint:disable:next identifier_name
+    @objc func _collectionView(_ collectionView: UICollectionView,
+                               willPerformUpdates updates: [UICollectionViewUpdateItem]) {
+        print("Diffable data source updates: \(updates)")
+    }
+}
+
+extension TaskListViewController {
+    typealias UICollectionViewDiffableDataSource = DebugDiffableDataSource
+}
+#endif
+
 struct Cell: View {
-    let data: Int
+    @Environment(\.managedObjectContext) private var viewContext
+
+    let data: String
+    let id: ObjectIdentifier
     @State var isExpanded: Bool = false
     @State var unexpandTask: Task<(), Never>?
 
     var body: some View {
-        Text("I'm a cell \(data), click me ")
+        VStack(alignment: .leading) {
+            Text("I'm a cell \(data)")
+            Text(id.debugDescription)
+                .font(.footnote)
+                .foregroundStyle(.secondary)
+        }
             .frame(minHeight: isExpanded ? 200 : 20)
-            .background(Color(hex: 0xF3FCDC * data))
+            .background(Color(hex: 0xF3FCDC * data.count))
             .clipShape(.rect(cornerRadius: 4.0))
             .animation(.bouncy(duration: 1), value: isExpanded)
             .onTapGesture {
@@ -124,6 +209,8 @@ struct Cell: View {
                         }
                     }
                 }
+
+                TasksData.addTask("New task added to view context", context: viewContext)
             }
     }
 }
