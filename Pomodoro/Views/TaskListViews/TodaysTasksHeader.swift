9bbf80f6654d27d3eb563fc5f75bcfc2ee5cda29
po-gl <porterglines@gmail.com>
2023-10-19 11:06:31 -0600

    Add header to collection view (boundary supplementary item)

---
+++ Pomodoro/Views/TaskListViews/TodaysTasksHeader.swift
@@ -0,0 +1,20 @@
+//
+//  TodaysTasksHeader.swift
+//  Pomodoro
+//
+//  Created by Porter Glines on 10/18/23.
+//
+
+import SwiftUI
+
+struct TodaysTasksHeader: View {
+    var body: some View {
+        HStack {
+            Text("Today's Tasks")
+                .textCase(.uppercase)
+                .font(.system(.footnote, weight: .medium))
+                .foregroundStyle(.secondary)
+            Spacer()
+        }
+    }
+}
