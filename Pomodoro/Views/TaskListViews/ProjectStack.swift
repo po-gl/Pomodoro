c0e1e293affc8744575c6464158ccf8ee3fe8c70
po-gl <porterglines@gmail.com>
2023-10-24 21:37:05 -0600

    Add functionality back to projects header (add project and collapse stack)
    Uses an observable object to communicate from UIKit to SwiftUI
    Also refactoring cell registers out of stored properties since they don't allow references to self

--- Pomodoro/Views/TaskListViews/ProjectStack.swift
+++ Pomodoro/Views/TaskListViews/ProjectStack.swift
@@ -15,7 +15,7 @@ struct ProjectStack: View {
                   predicate: NSPredicate(format: "archived == false"))
     private var currentProjects: FetchedResults<Project>
 
-    @State var isCollapsed = true
+    @ObservedObject var isCollapsed: ObservableBool
 
     let collapsedRowHeight: Double = 85
 
@@ -25,26 +25,26 @@ struct ProjectStack: View {
                 ForEach(0..<currentProjects.count, id: \.self) { i in
                     let iDouble = Double(i)
                     ProjectCell(project: currentProjects[i],
-                                isCollapsed: $isCollapsed,
+                                isCollapsed: isCollapsed,
                                 cellHeight: collapsedRowHeight,
                                 isFirstProject: i == 0)
                     .zIndex(-iDouble)
-                    .opacity(isCollapsed ? 1 - (0.3 * iDouble) : 1.0)
-                    .scaleEffect(isCollapsed ? 1 - (0.08 * iDouble) : 1.0)
-                    .offset(y: isCollapsed ? -iDouble * collapsedRowHeight + (iDouble * 3) : 0.0)
+                    .opacity(isCollapsed.value ? 1 - (0.3 * iDouble) : 1.0)
+                    .scaleEffect(isCollapsed.value ? 1 - (0.08 * iDouble) : 1.0)
+                    .offset(y: isCollapsed.value ? -iDouble * collapsedRowHeight + (iDouble * 3) : 0.0)
                     .compositingGroup()
                 }
             }
-            .frame(maxHeight: isCollapsed ? collapsedRowHeight*1.25 : .infinity, alignment: .top)
+            .frame(maxHeight: isCollapsed.value ? collapsedRowHeight*1.25 : .infinity, alignment: .top)
             .padding(.top, 10)
         }
-        .animation(.easeInOut, value: isCollapsed)
+        .animation(.easeInOut, value: isCollapsed.value)
     }
 }
 
 #Preview {
     ScrollView {
-        ProjectStack()
+        ProjectStack(isCollapsed: ObservableBool(true))
             .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
--- Pomodoro/Views/TaskListViews/ProjectsHeader.swift
+++ Pomodoro/Views/TaskListViews/ProjectsHeader.swift
@@ -8,13 +8,60 @@
 import SwiftUI
 
 struct ProjectsHeader: View {
+    @Environment(\.managedObjectContext) var viewContext
+    @Environment(\.colorScheme) var colorScheme
+
+    @ObservedObject var isCollapsed: ObservableBool
+
     var body: some View {
-        HStack {
+        HStack(spacing: 20) {
             Text("Projects")
                 .textCase(.uppercase)
                 .font(.system(.footnote, weight: .medium))
                 .foregroundStyle(.secondary)
             Spacer()
+            Group {
+                projectHeaderAddButton()
+                projectHeaderChevronButton()
+            }
+            .opacity(isCollapsed.value ? 0.0 : 1.0)
+        }
+    }
+
+    @ViewBuilder
+    private func projectHeaderAddButton() -> some View {
+        Button(action: {
+            withAnimation {
+                ProjectsData.addProject("", context: viewContext)
+            }
+        }) {
+            Text("Add Project")
+                .textCase(.uppercase)
+                .font(.system(.footnote, weight: .medium))
+                .foregroundColor(Color("BarRest"))
+                .brightness(colorScheme == .dark ? 0.4 : -0.1)
+                .saturation(colorScheme == .dark ? 1.5 : 0.9)
+        }
+    }
+
+    @ViewBuilder
+    private func projectHeaderChevronButton() -> some View {
+        Button(action: {
+            withAnimation(.spring()) {
+                isCollapsed.value = true
+            }
+        }) {
+            Image(systemName: "chevron.compact.up")
+                .font(.system(size: 26))
+                .foregroundColor(Color("BarRest"))
+                .brightness(colorScheme == .dark ? 0.4 : -0.1)
+                .saturation(colorScheme == .dark ? 1.5 : 0.9)
+                .padding(.vertical, 2).padding(.horizontal, 4)
+                .background(
+                    RoundedRectangle(cornerRadius: 8).fill(Color("BarRest"))
+                        .brightness(colorScheme == .dark ? -0.5 : 0.3)
+                        .saturation(colorScheme == .dark ? 0.8 : 0.7)
+                )
         }
     }
 }
