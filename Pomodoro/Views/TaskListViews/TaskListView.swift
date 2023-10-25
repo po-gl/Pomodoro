25e1e23e06bcf9e1e8bdb0ef8cdcaba241c942ef
po-gl <porterglines@gmail.com>
2023-10-24 19:07:06 -0600

    Add toolbar buttons to task list wrapper

--- Pomodoro/Views/TaskListViews/TaskListView.swift
+++ Pomodoro/Views/TaskListViews/TaskListView.swift
@@ -10,9 +10,19 @@ import SwiftUI
 struct TaskListView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
+    @State var showingArchivedProjects = false
+    @AppStorage("showPastTasks") var showPastTasks = true
+
+    @FetchRequest(fetchRequest: TasksData.todaysTasksRequest)
+    var todaysTasks: FetchedResults<TaskNote>
+
+    @FetchRequest(fetchRequest: TasksData.yesterdaysTasksRequest)
+    var yesterdaysTasks: FetchedResults<TaskNote>
+
     var body: some View {
-        TaskListCollectionView()
-            .navigationTitle("New List Style")
+        TaskListCollectionView(showPastTasks: $showPastTasks)
+            .ignoresSafeArea(.keyboard)
+            .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .bottomBar) {
                     Button("Add task") {
@@ -20,7 +30,76 @@ struct TaskListView: View {
                     }
                 }
             }
-            .ignoresSafeArea(.keyboard)
+            .toolbar {
+                Menu {
+                    showArchivedProjectsButton()
+                    Divider()
+                    showPastTasksButton()
+                    markTodaysTasksAsDoneButton()
+                    addYesterdaysUnfinishedTasksButton()
+                } label: {
+                    Image(systemName: "ellipsis.circle")
+                }
+                .navigationDestination(isPresented: $showingArchivedProjects) {
+                    ArchivedProjectsView()
+                }
+            }
+    }
+
+    @ViewBuilder
+    private func showArchivedProjectsButton() -> some View {
+        Button(action: {
+            basicHaptic()
+            showingArchivedProjects = true
+        }) {
+            Label("Show Archived Projects", systemImage: "eye.fill")
+        }
+    }
+
+    @ViewBuilder
+    private func showPastTasksButton() -> some View {
+        Button(action: {
+            basicHaptic()
+            withAnimation { showPastTasks.toggle() }
+        }) {
+            if showPastTasks {
+                Label("Hide Past Tasks", systemImage: "eye.slash")
+            } else {
+                Label("Show Past Tasks", systemImage: "eye")
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func markTodaysTasksAsDoneButton() -> some View {
+        Button(action: {
+            todaysTasks.forEach { TasksData.setCompleted(for: $0, context: viewContext) }
+        }) {
+            Label("Mark Today as Done", systemImage: "checklist.checked")
+        }
+    }
+
+    @ViewBuilder
+    private func addYesterdaysUnfinishedTasksButton() -> some View {
+        Button(action: {
+            yesterdaysTasks
+                .filter({ !$0.completed })
+                .filter({ task in !todaysTasks.contains(where: { $0.text == task.text })})
+                .forEach { taskToAdd in
+                if let taskText = taskToAdd.text {
+                    withAnimation {
+                        TasksData.addTask(taskText,
+                                          note: taskToAdd.note ?? "",
+                                          flagged: taskToAdd.flagged,
+                                          date: Date().addingTimeInterval(-1),
+                                          context: viewContext)
+                    }
+                }
+            }
+
+        }) {
+            Label("Add Unfinished Tasks", systemImage: "arrow.uturn.up")
+        }
     }
 }
 
