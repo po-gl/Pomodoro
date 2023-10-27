dc0c4218b7814e372f189787490627c4b212d05d
po-gl <porterglines@gmail.com>
2023-10-27 16:25:23 -0600

    Add view modifier to override view tap actions

---
+++ Pomodoro/Extensions/View+overrideAction.swift
@@ -0,0 +1,32 @@
+//
+//  View+overrideAction.swift
+//  Pomodoro
+//
+//  Created by Porter Glines on 10/27/23.
+//
+
+import SwiftUI
+
+extension View {
+    func overrideAction(predicate: Bool, newAction: @escaping () -> Void) -> some View {
+        ModifiedContent(content: self, modifier: OverrideActionViewModifier(predicate: predicate,
+                                                                            action: newAction))
+    }
+}
+
+struct OverrideActionViewModifier: ViewModifier {
+    var predicate = false
+    var action: () -> Void = {}
+
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+                .disabled(predicate)
+            if predicate {
+                Color.clear
+                    .contentShape(Rectangle())
+                    .onTapGesture(perform: action)
+            }
+        }
+    }
+}
