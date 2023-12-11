135fbb86a91b7bb4e939ac1789ec3ece2c8dca2a
po-gl <porterglines@gmail.com>
2023-12-10 22:42:34 -0700

    Revamp iOS widget design

--- iOSWidget/iOSWidget.swift
+++ iOSWidget/iOSWidget.swift
@@ -23,6 +23,8 @@ struct iOSWidget: Widget {
 }
 
 struct iOSWidgetView: View {
+    @Environment(\.widgetFamily) var widgetFamily
+    
     var entry: WidgetTimelineProvider.Entry
 
     var body: some View {
@@ -30,10 +32,16 @@ struct iOSWidgetView: View {
             content
                 .containerBackground(for: .widget) {
                     Color.black
+                    LinearGradient(colors: [entry.status.color, .clear], startPoint: .top, endPoint: .bottom)
+                        .opacity(entry.isPaused ? 0.13 : 0.2)
+                        .animation(.default, value: entry.isPaused)
                 }
         } else {
             ZStack {
-                Rectangle().foregroundColor(.black)
+                Rectangle().fill(.black)
+                LinearGradient(colors: [entry.status.color, .clear], startPoint: .top, endPoint: .bottom)
+                    .opacity(entry.isPaused ? 0.13 : 0.2)
+                    .animation(.default, value: entry.isPaused)
                 content
             }
             .ignoresSafeArea()
@@ -41,22 +49,113 @@ struct iOSWidgetView: View {
     }
 
     @ViewBuilder var content: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            Text("\(entry.status.rawValue)")
-                .font(.title2).fontWeight(.light).fontDesign(.monospaced)
-                .foregroundStyle(entry.status.color)
-            HStack(spacing: 8) {
-                Text(timerInterval: entry.timerInterval, pauseTime: entry.isPaused ? entry.timerInterval.lowerBound : nil)
-                    .foregroundStyle(.white)
-                    .font(.title).fontWeight(.regular)
-                    .monospacedDigit()
-                Text(entry.status.icon)
-                    .font(.system(size: 20))
-                    .foregroundStyle(entry.status.color)
-            }
+        if widgetFamily == .systemSmall {
+            smallLayout
+        } else {
+            mediumLayout
         }
     }
+
+    @ViewBuilder var mediumLayout: some View {
+        VStack(alignment: .trailing, spacing: 0) {
+            Spacer()
+            HStack(spacing: 0) {
+                VStack(alignment:. leading, spacing: 5) {
+                    statusView
+                    timerEndView
+                        .offset(x: 4)
+                }
+                Spacer()
+                timerView
+                    .offset(y: -5)
+            }
+            Spacer()
+            WidgetProgressBar(timerInterval: entry.timerInterval,
+                              currentSegment: entry.currentSegment,
+                              segmentCount: entry.segmentCount,
+                              pausedAt: entry.isPaused ? entry.timerInterval.lowerBound : nil)
+            .frame(height: 5)
+        }
+    }
+
+    @ViewBuilder var smallLayout: some View {
+        VStack(alignment: .trailing, spacing: 0) {
+            timerView
+                .offset(y: -5)
+            Spacer()
+            HStack(spacing: 0) {
+                VStack(alignment:. leading, spacing: 5) {
+                    statusView
+                    timerEndView
+                        .offset(x: 4)
+                }
+                Spacer()
+            }
+            Spacer()
+            WidgetProgressBar(timerInterval: entry.timerInterval,
+                              currentSegment: entry.currentSegment,
+                              segmentCount: entry.segmentCount,
+                              pausedAt: entry.isPaused ? entry.timerInterval.lowerBound : nil)
+            .frame(height: 5)
+        }
+    }
+
+    @ViewBuilder var timerView: some View {
+        let startDate = entry.timerInterval.lowerBound
+        let endDate = entry.timerInterval.upperBound
+        if entry.isPaused {
+            Text(endDate.timeIntervalSince(startDate).compactTimerFormatted())
+                .font(.system(size: 42, weight: .light))
+                .monospacedDigit()
+                .frame(width: 115, alignment: .trailing)
+                .foregroundStyle(.white)
+        } else {
+            Text(timerInterval: entry.timerInterval, countsDown: true)
+                .multilineTextAlignment(.trailing)
+                .font(.system(size: 42, weight: .light))
+                .monospacedDigit()
+                .frame(width: 115)
+                .foregroundStyle(.white)
+                .contentTransition(.numericText(countsDown: true))
+        }
+    }
+
+    @ViewBuilder var statusView: some View {
+        let task = entry.task ?? ""
+        Text(task != "" ? task : entry.status.rawValue)
+            .font(.system(.headline, design: .rounded, weight: .medium))
+            .lineLimit(widgetFamily == .systemSmall ? 1 : 3)
+            .foregroundStyle(.black)
+            .padding(.horizontal, 5)
+            .background(
+                RoundedRectangle(cornerRadius: 5)
+                    .foregroundStyle(entry.status.color)
+                    .brightness(0.2)
+                    .shadow(radius: 2, x: 2, y: 2)
+                    .background(
+                        RoundedRectangle(cornerRadius: 5)
+                            .offset(x: 3, y: 3)
+                            .foregroundStyle(entry.status.color)
+                            .brightness(0.0)
+                    )
+            )
+    }
+
+    @ViewBuilder var timerEndView: some View {
+        let endTime = entry.isPaused ? "--:--" : timeFormatter.string(from: entry.timerInterval.lowerBound)
+        Text("until \(endTime)")
+            .font(.system(.subheadline, design: .rounded, weight: .regular))
+            .monospacedDigit()
+            .opacity(0.6)
+            .foregroundStyle(.white)
+    }
 }
+
+private let timeFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.setLocalizedDateFormatFromTemplate("hh:mm")
+    return formatter
+}()
 
 struct iOSWidget_Previews: PreviewProvider {
     static var pomoTimer = PomoTimer(pomos: 2, longBreak: PomoTimer.defaultBreakTime, perform: { _ in return })
@@ -68,7 +167,7 @@ struct iOSWidget_Previews: PreviewProvider {
                                       task: nil,
                                       timerInterval: timerInterval,
                                       isPaused: false,
-                                      currentSegment: 2,
+                                      currentSegment: 5,
                                       segmentCount: 6,
                                       configuration: ConfigurationIntent())
         Group {
