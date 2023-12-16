c328b52a63e4869c89767af14c15f0ebf1666ff9
po-gl <porterglines@gmail.com>
2023-12-16 03:00:24 -0700

    Add string convertible extension to ScenePhase

---
+++ Shared/Extensions/ScenePhase+CustomStringConvertible.swift
@@ -0,0 +1,23 @@
+//
+//  ScenePhase+CustomStringConvertible.swift
+//  Pomodoro
+//
+//  Created by Porter Glines on 12/16/23.
+//
+
+import SwiftUI
+
+extension ScenePhase: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .active:
+            "active"
+        case .inactive:
+            "inactive"
+        case .background:
+            "background"
+        @unknown default:
+            "unknown"
+        }
+    }
+}
