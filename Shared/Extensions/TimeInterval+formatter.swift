425937da80983ac4b26e06d62b24a0a5fe0f20a3
po-gl <porterglines@gmail.com>
2024-01-03 01:09:58 -0700

    Dynamically display hour portion in compactTimerFormatted()

--- Shared/Extensions/TimeInterval+formatter.swift
+++ Shared/Extensions/TimeInterval+formatter.swift
@@ -19,11 +19,15 @@ extension TimeInterval {
         return TimeInterval.formatter.string(from: self)!
     }
 
-    /// Formats time in "m:ss"
+    /// Formats time in "m:ss" or dynamically "h:mm:ss"
     func compactTimerFormatted() -> String {
         let totalSeconds = Int(self)
+        let hours = totalSeconds / 3600
         let minutes = (totalSeconds % 3600) / 60
         let seconds = totalSeconds % 60
-        return "\(minutes):\(seconds < 10 ? "0" : "")\(seconds)"
+        let hourPortion = hours > 0 ? "\(hours):" : ""
+        let minutesPortion = minutes < 10 && hours > 0 ? "0\(minutes):" : "\(minutes):"
+        let secondsPortion = seconds < 10 ? "0\(seconds)" : "\(seconds)"
+        return hourPortion + minutesPortion + secondsPortion
     }
 }
