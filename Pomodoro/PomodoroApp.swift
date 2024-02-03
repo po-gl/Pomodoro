67584f2cfe23bb61b4fddc45eaf7dbaafe4122f0
po-gl <porterglines@gmail.com>
2024-02-03 01:51:15 -0700

    Move lifecycle code and core objects and their subscribers up to PomodoroApp

--- Pomodoro/PomodoroApp.swift
+++ Pomodoro/PomodoroApp.swift
@@ -6,6 +6,12 @@
 //
 
 import SwiftUI
+import WidgetKit
+#if canImport(ActivityKit)
+import ActivityKit
+#endif
+import WatchConnectivity
+import Combine
 import OSLog
 
 @main
@@ -13,10 +19,118 @@ struct PomodoroApp: App {
     @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
     let persistenceController = PersistenceController.shared
 
+    @Environment(\.scenePhase) private var scenePhase
+    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.undoManager) private var undoManager
+
+    @ObservedObject var pomoTimer: PomoTimer
+    @StateObject var tasksOnBar = TasksOnBar.shared
+
+    @State var didReceiveSyncFromWatchConnection = false
+    @State var didPerformInactiveSetup = false
+
+    init() {
+        let workDuration = UserDefaults.pomo?.value(forKey: "workDuration") as? Double ?? PomoTimer.defaultWorkTime
+        let restDuration = UserDefaults.pomo?.value(forKey: "restDuration") as? Double ?? PomoTimer.defaultRestTime
+        let breakDuration = UserDefaults.pomo?.value(forKey: "breakDuration") as? Double ?? PomoTimer.defaultBreakTime
+        pomoTimer = PomoTimer(pomos: 4, work: workDuration, rest: restDuration, longBreak: breakDuration) { status in
+            EndTimerHandler.shared.handle(status: status)
+        }
+        pomoTimer.restoreFromUserDefaults()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(pomoTimer)
+                .environmentObject(tasksOnBar)
+                .onAppear {
+                    AppNotifications.shared.getNotificationPermissions()
+                    viewContext.undoManager = undoManager
+                }
+
+                .onChange(of: scenePhase) { newPhase in
+                    Logger().log("Phase \(newPhase)")
+                    if newPhase == .active {
+                        pomoTimer.restoreFromUserDefaults()
+                        AppNotifications.shared.cancelPendingNotifications()
+                        setupWatchConnection()
+                        didPerformInactiveSetup = false
+                        if #available(iOS 16.2, *) {
+#if canImport(ActivityKit)
+                            LiveActivities.shared.startPollingPushTokenUpdates()
+#endif
+                        }
+                        UIApplication.shared.registerForRemoteNotifications()
+
+                    } else if newPhase == .inactive || newPhase == .background {
+                        guard !didPerformInactiveSetup else { return }
+                        pomoTimer.saveToUserDefaults()
+                        WidgetCenter.shared.reloadAllTimelines()
+                        if !didReceiveSyncFromWatchConnection {
+                            Task { await AppNotifications.shared.setupNotifications(pomoTimer) }
+                        }
+                        didPerformInactiveSetup = true
+                    }
+                }
+
+                .onChange(of: pomoTimer.isPaused) { isPaused in
+                    WidgetCenter.shared.reloadAllTimelines()
+                    let wcSent = updateWatchConnection(pomoTimer)
+                    didReceiveSyncFromWatchConnection = !wcSent
+                    UIApplication.shared.registerForRemoteNotifications()
+
+                    if #available(iOS 16.2, *) {
+#if canImport(ActivityKit)
+                        if isPaused {
+                            LiveActivities.shared.stopLiveActivity(pomoTimer, tasksOnBar)
+                        } else {
+                            LiveActivities.shared.setupLiveActivity(pomoTimer, tasksOnBar)
+                        }
+#endif
+                    }
+#if targetEnvironment(macCatalyst)
+                    if !isPaused {
+                        Task { await AppNotifications.shared.setupNotifications(pomoTimer) }
+                    }
+#endif
+                }
+                .onChange(of: pomoTimer.isReset) { isReset in
+                    if isReset {
+                        WidgetCenter.shared.reloadAllTimelines()
+                        let wcSent = updateWatchConnection(pomoTimer)
+                        didReceiveSyncFromWatchConnection = !wcSent
+                        
+                        if #available(iOS 16.2, *) {
+#if canImport(ActivityKit)
+                            LiveActivities.shared.stopLiveActivity(pomoTimer, tasksOnBar)
+#endif
+                        }
+                    }
+                }
+
+                .onReceive(Publishers.wcSessionDataDidFlow) { timer in
+                    if let timer {
+                        Logger().debug("iOS received pomoTimer.pomoCount=\(timer.pomoCount) isPaused=\(timer.isPaused)")
+                        pomoTimer.sync(with: timer)
+                        pomoTimer.saveToUserDefaults()
+                        didReceiveSyncFromWatchConnection = true
+                    }
+                }
+
+                .onOpenURL { url in
+                    switch url.absoluteString {
+                    case "com.po-gl.pause":
+                        pomoTimer.pause()
+                        pomoTimer.saveToUserDefaults()
+                    case "com.po-gl.unpause":
+                        pomoTimer.unpause()
+                        pomoTimer.saveToUserDefaults()
+                    default:
+                        Logger().error("Unhandled url: \(url.absoluteString)")
+                    }
+                }
 #if targetEnvironment(macCatalyst)
                 .frame(minWidth: 600, idealWidth: 700, minHeight: 900, idealHeight: 1000)
 #endif
--- Pomodoro/Views/ContentView.swift
+++ Pomodoro/Views/ContentView.swift
@@ -6,43 +6,14 @@
 //
 
 import SwiftUI
-import CoreHaptics
-import WidgetKit
-#if canImport(ActivityKit)
-import ActivityKit
-#endif
-import WatchConnectivity
 import Combine
-import OSLog
 
 struct ContentView: View {
-    @Environment(\.scenePhase) private var scenePhase
-    @Environment(\.colorScheme) private var colorScheme
-
-    @Environment(\.managedObjectContext) private var viewContext
-    @Environment(\.undoManager) private var undoManager
-
     @AppStorage("shouldOnBoard", store: UserDefaults.pomo) var shouldOnBoard = true
 
     @ObservedObject var errors = Errors.shared
 
-    @ObservedObject var pomoTimer: PomoTimer
-    @StateObject var tasksOnBar = TasksOnBar.shared
-
-    @State var didReceiveSyncFromWatchConnection = false
-    @State var didPerformInactiveSetup = false
-
     @State var selectedTab = 0
-
-    init() {
-        let workDuration = UserDefaults.pomo?.value(forKey: "workDuration") as? Double ?? PomoTimer.defaultWorkTime
-        let restDuration = UserDefaults.pomo?.value(forKey: "restDuration") as? Double ?? PomoTimer.defaultRestTime
-        let breakDuration = UserDefaults.pomo?.value(forKey: "breakDuration") as? Double ?? PomoTimer.defaultBreakTime
-        pomoTimer = PomoTimer(pomos: 4, work: workDuration, rest: restDuration, longBreak: breakDuration) { status in
-            EndTimerHandler.shared.handle(status: status)
-        }
-        pomoTimer.restoreFromUserDefaults()
-    }
 
     var body: some View {
         TabView(selection: $selectedTab) {
@@ -61,101 +32,13 @@ struct ContentView: View {
                 .tabItem { Label { Text("Settings") } icon: { Image(.pomoGear) } }
                 .tag(2)
         }
-        .environmentObject(pomoTimer)
-        .environmentObject(tasksOnBar)
         .onReceive(Publishers.selectFirstTab) { _ in
             selectedTab = 0
-        }
-        .onAppear {
-            AppNotifications.shared.getNotificationPermissions()
-            viewContext.undoManager = undoManager
         }
 
         .sheet(isPresented: $shouldOnBoard) {
             OnBoardView()
                 .presentationDragIndicator(.visible)
-        }
-
-        .onChange(of: scenePhase) { newPhase in
-            Logger().log("Phase \(newPhase)")
-            if newPhase == .active {
-                pomoTimer.restoreFromUserDefaults()
-                AppNotifications.shared.cancelPendingNotifications()
-                setupWatchConnection()
-                didPerformInactiveSetup = false
-                if #available(iOS 16.2, *) {
-#if canImport(ActivityKit)
-                    LiveActivities.shared.startPollingPushTokenUpdates()
-#endif
-                }
-                UIApplication.shared.registerForRemoteNotifications()
-                
-            } else if newPhase == .inactive || newPhase == .background {
-                guard !didPerformInactiveSetup else { return }
-                pomoTimer.saveToUserDefaults()
-                WidgetCenter.shared.reloadAllTimelines()
-                if !didReceiveSyncFromWatchConnection {
-                    Task { await AppNotifications.shared.setupNotifications(pomoTimer) }
-                }
-                didPerformInactiveSetup = true
-            }
-        }
-        
-        .onChange(of: pomoTimer.isPaused) { isPaused in
-            WidgetCenter.shared.reloadAllTimelines()
-            let wcSent = updateWatchConnection(pomoTimer)
-            didReceiveSyncFromWatchConnection = !wcSent
-            UIApplication.shared.registerForRemoteNotifications()
-
-            if #available(iOS 16.2, *) {
-#if canImport(ActivityKit)
-                if isPaused {
-                    LiveActivities.shared.stopLiveActivity(pomoTimer, tasksOnBar)
-                } else {
-                    LiveActivities.shared.setupLiveActivity(pomoTimer, tasksOnBar)
-                }
-#endif
-            }
-#if targetEnvironment(macCatalyst)
-            if !isPaused {
-                Task { await AppNotifications.shared.setupNotifications(pomoTimer) }
-            }
-#endif
-        }
-        .onChange(of: pomoTimer.isReset) { isReset in
-            if isReset {
-                WidgetCenter.shared.reloadAllTimelines()
-                let wcSent = updateWatchConnection(pomoTimer)
-                didReceiveSyncFromWatchConnection = !wcSent
-                
-                if #available(iOS 16.2, *) {
-#if canImport(ActivityKit)
-                    LiveActivities.shared.stopLiveActivity(pomoTimer, tasksOnBar)
-#endif
-                }
-            }
-        }
-        
-        .onReceive(Publishers.wcSessionDataDidFlow) { timer in
-            if let timer {
-                Logger().debug("iOS received pomoTimer.pomoCount=\(timer.pomoCount) isPaused=\(timer.isPaused)")
-                pomoTimer.sync(with: timer)
-                pomoTimer.saveToUserDefaults()
-                didReceiveSyncFromWatchConnection = true
-            }
-        }
-        
-        .onOpenURL { url in
-            switch url.absoluteString {
-            case "com.po-gl.pause":
-                pomoTimer.pause()
-                pomoTimer.saveToUserDefaults()
-            case "com.po-gl.unpause":
-                pomoTimer.unpause()
-                pomoTimer.saveToUserDefaults()
-            default:
-                Logger().error("Unhandled url: \(url.absoluteString)")
-            }
         }
     }
 }
@@ -167,6 +50,12 @@ struct ContentView_Previews: PreviewProvider {
                 .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
                 .previewDevice(PreviewDevice(rawValue: device))
                 .previewDisplayName(device)
+                .environmentObject(
+                    PomoTimer() { status in
+                        EndTimerHandler.shared.handle(status: status)
+                    }
+                )
+                .environmentObject(TasksOnBar.shared)
         }
     }
 }
