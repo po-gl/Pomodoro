7ae868d3c4ce8088fa6d98a107c53e3372842887
po-gl <porterglines@gmail.com>
2024-02-13 18:59:48 -0700

    Remove files unnecessary to target membership in UITest and Test targets

--- PomodoroTests/PomoTimeTests.swift
+++ PomodoroTests/PomoTimeTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import Pomodoro
 
 final class PomoTimeTests: XCTestCase {
 
--- PomodoroTests/PomoTimerTests.swift
+++ PomodoroTests/PomoTimerTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import Pomodoro
 
 final class PomoTimerTests: XCTestCase {
 
