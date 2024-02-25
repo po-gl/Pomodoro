bc4fc3108ab3f1e393b7f42607f35eb58140faa4
po-gl <porterglines@gmail.com>
2024-02-24 22:32:49 -0700

    Add function to add preview cumulative times

--- Pomodoro/Model/CumulativeTimeData.swift
+++ Pomodoro/Model/CumulativeTimeData.swift
@@ -144,4 +144,57 @@ struct CumulativeTimeData {
             Logger().error("\(errorMessage) (synchronous) : \(error), \(error.userInfo)")
         }
     }
+
+    static func addPreviewTimes(between: ClosedRange<Date>, addRandomVariance: Bool = false, context: NSManagedObjectContext) {
+        let patternVariance = addRandomVariance ? Double.random(in: 1.0...8.0) : 0.0
+        let pattern: [(PomoStatus, TimeInterval)] = [
+            (.work, PomoTimer.defaultWorkTime + (patternVariance * 60)),
+            (.rest, PomoTimer.defaultRestTime + (patternVariance / 2 * 60)),
+            (.work, PomoTimer.defaultWorkTime + (patternVariance * 60)),
+            (.rest, PomoTimer.defaultRestTime + (patternVariance / 2 * 60)),
+            (.longBreak, PomoTimer.defaultBreakTime),
+        ]
+        print("Pattern: \(pattern)")
+        var times = [(PomoStatus, TimeInterval)]()
+
+        var i = 0
+        var date = between.lowerBound.startOfHour
+        var hourAccumulator = 0.0
+        while date < between.upperBound.startOfHour {
+            let startOfHour = date.startOfHour
+
+            let timeToAdd = pattern[i % pattern.count].1
+            times.append(pattern[i % pattern.count])
+            hourAccumulator += timeToAdd
+            print("\(hourAccumulator.rounded()) times: \(times)")
+
+            if hourAccumulator > 3600 {
+                let excess = hourAccumulator.truncatingRemainder(dividingBy: 3600)
+                print("Excess: \(excess) for \(times[times.count-1].0)")
+                times[times.count-1].1 -= excess
+
+                let workTime = times.reduce(0.0, { $0 + ($1.0 == .work ? $1.1 : 0.0)})
+                let restTime = times.reduce(0.0, { $0 + ($1.0 == .rest ? $1.1 : 0.0)})
+                let breakTime = times.reduce(0.0, { $0 + ($1.0 == .longBreak ? $1.1 : 0.0)})
+                print("Adding time (\(workTime.rounded()), \(restTime.rounded()), \(breakTime.rounded())) sum = \((workTime+restTime+breakTime).rounded()) for \(startOfHour.formatted())")
+                CumulativeTimeData.addTime(work: workTime, rest: restTime, longBreak: breakTime,
+                                           date: startOfHour, context: context)
+
+                let endStatus = times[times.count-1].0
+                times = []
+                hourAccumulator -= 3600
+                times.append((status: endStatus, time: excess))  // add back excess
+            }
+            i += 1
+            date.addTimeInterval(timeToAdd)
+        }
+        print("At End ===")
+        print("\(hourAccumulator.rounded()) times: \(times)")
+        let workTime = times.reduce(0.0, { $0 + ($1.0 == .work ? $1.1 : 0.0)})
+        let restTime = times.reduce(0.0, { $0 + ($1.0 == .rest ? $1.1 : 0.0)})
+        let breakTime = times.reduce(0.0, { $0 + ($1.0 == .longBreak ? $1.1 : 0.0)})
+        CumulativeTimeData.addTime(work: workTime, rest: restTime, longBreak: breakTime,
+                                   date: between.upperBound, context: context)
+        print("Adding time (\(workTime.rounded()), \(restTime.rounded()), \(breakTime.rounded())) sum=\((workTime+restTime+breakTime).rounded()) for \(between.upperBound.formatted())")
+    }
 }
